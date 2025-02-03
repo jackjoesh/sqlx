c6d2fe0b371139d5dca6098b490c4fdb0666561f
jerry.l <jerry.l@binance.com>
2025-02-03 15:21:01 +0800

    public column type

--- sqlx-mysql/src/lib.rs
+++ sqlx-mysql/src/lib.rs
@@ -39,6 +39,7 @@ mod testing;
 
 pub use arguments::MySqlArguments;
 pub use column::MySqlColumn;
+pub use protocol::text::ColumnType;
 pub use connection::MySqlConnection;
 pub use database::MySql;
 pub use error::MySqlDatabaseError;
